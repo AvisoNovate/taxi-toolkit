aa39ddf6ddebf463f75f7deffd213304e3fc41ab
Tomek Lipski <tomek.lipski@gmail.com>
2015-10-22 21:27:20 +0200

    extract sub functions from remote-session-fixture

--- src/io/aviso/taxi_toolkit/fixtures.clj
+++ src/io/aviso/taxi_toolkit/fixtures.clj
@@ -5,50 +5,64 @@
             [clj-webdriver.remote.server :as webdriver-remote])
   (:import (java.util.concurrent TimeUnit)))
 
-(defn webdriver-remote-fixture
-  "The first argument should be a timeout in milliseconds that will be applied to scriptTimeout and pageLoadTimeout of the driver.
+(defn new-webdriver-remote-session
+"Set up a webdriver remote session for a port/host specified as:
+- system properties - named selenium.port/selenium.host or
+- environment variables - named SELENIUM_PORT/SELENIUM_HOST
 
-All other arguments are browser specs, and for each of them:
-
-First, set up webdriver remote session for a port and a host specified as:
-- system properties - selenium.port/selenium.host or
-- environment variables - SELENIUM_PORT/SELENIUM_HOST
 If SELENIUM_HOST is used, and SAUCE_USER_NAME environment variable is set, set the host
-to include the username:password@host authentication string.
+will include the authentication string in a following format: username:password@host.
 
-This follows the convention from: https://github.com/saucelabs/bamboo_sauce/wiki#referencing-environment-variables to some extent and
-the default settings are localhost/4444.
+This follows the convention from: https://github.com/saucelabs/bamboo_sauce/wiki#referencing-environment-variables to some extent.
 
-Then, set the default browser size to 1680x1050 for each iteration.
+The default settings are localhost/4444."
+  [browser-spec]
+  (second
+    (webdriver-remote/new-remote-session
+     {:port     (Integer/parseInt (or
+                                    (System/getProperty "selenium.port")
+                                    (System/getenv "SELENIUM_PORT")
+                                    "4444"))
+      :existing true
+      :host     (or
+                  (System/getProperty "selenium.host")
+                  (when-let [host (System/getenv "SELENIUM_HOST")]
+                    (if (System/getenv "SAUCE_USER_NAME")
+                      (str (System/getenv "SAUCE_USER_NAME") ":"
+                           (System/getenv "SAUCE_API_KEY") "@"
+                           host)
+                      host))
+                  "localhost")}
+     browser-spec)))
 
-Next, invoke the function provided (assuming - the tests).
+(defn setup-webdriver-remote-session!
+"Configure webdriver remote session, setting the timeout and resizing the browser window."
+  [a-driver timeout-ms window-size]
+  (.setScriptTimeout (.. (:webdriver a-driver) manage timeouts) timeout-ms TimeUnit/MILLISECONDS) ;meh meh meh
+  (.pageLoadTimeout (.. (:webdriver a-driver) manage timeouts) timeout-ms TimeUnit/MILLISECONDS) ;meh meh meh
+  (taxi/implicit-wait 200)                            ;meh
+  (taxi/window-resize window-size))
 
-Finally, quit the webdriver session."
-  [timeout-ms & browser-specs]
+(defn webdriver-remote-fixture
+"Takes options map as first argument, which can have the following entries:
+
+  - timeout-ms - defaults to 15000
+  - window-size - defaults to {:width 1680 :height 1050}
+
+Other arguments are browser specs, and for each of them, the driver session is created
+using new-webdriver-remote-session, set up with setup-webdriver-remote-session!.
+
+Next, the fixture function is invoked as usual.
+
+Finally, quit function is invoked on the webdriver session to close it."
+  [{:keys [timeout-ms window-size] :or {timeout-ms 15000 window-size {:width 1680 :height 1050}}}
+   & browser-specs]
   (fn [f]
     (doseq [browser-spec browser-specs]
-      (let [[_ a-driver] (webdriver-remote/new-remote-session
-                           {:port     (Integer/parseInt (or
-                                                          (System/getProperty "selenium.port")
-                                                          (System/getenv "SELENIUM_PORT")
-                                                          "4444"))
-                            :existing true
-                            :host     (or
-                                        (System/getProperty "selenium.host")
-                                        (when-let [host (System/getenv "SELENIUM_HOST")]
-                                          (if (System/getenv "SAUCE_USER_NAME")
-                                            (str (System/getenv "SAUCE_USER_NAME") ":"
-                                                 (System/getenv "SAUCE_API_KEY") "@"
-                                                 host)
-                                            host))
-                                        "localhost")}
-                           browser-spec)]
-        (taxi/set-driver! a-driver)
-        (.setScriptTimeout (.. (:webdriver a-driver) manage timeouts) timeout-ms TimeUnit/MILLISECONDS) ;meh meh meh
-        (.pageLoadTimeout (.. (:webdriver a-driver) manage timeouts) timeout-ms TimeUnit/MILLISECONDS) ;meh meh meh
-        (taxi/implicit-wait 200)                            ;meh
+      (let [a-driver (new-webdriver-remote-session browser-spec)]
         (try
-          (taxi/window-resize {:width 1680 :height 1050})
+          (taxi/set-driver! a-driver)
+          (setup-webdriver-remote-session! a-driver timeout-ms window-size)
           (f)
           (finally (taxi/quit)))))))
 
@@ -56,8 +70,7 @@ Finally, quit the webdriver session."
 "General purpose fixture, in case Selenium tests would hang (it happens).
 
 Takes one argument:
-- timeout-seconds - timeout in seconds after which the JVM should be stopped if the
-test is still running."
+- timeout-seconds - timeout in seconds after which the JVM should be stopped if the test is still running."
   [timeout-seconds]
   (fn [f]
     (let [finished? (atom false)]
