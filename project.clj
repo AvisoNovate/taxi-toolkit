415c30cac129e4610e2b52cd5d086e705d7c810c
Kamil Leszczuk <kamituel@gmail.com>
2016-05-06 13:34:15 +0200

    Mark next dev release 0.2.6-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.aviso/taxi-toolkit "0.2.5"
+(defproject io.aviso/taxi-toolkit "0.2.6-SNAPSHOT"
   :description "A Clojure library designed to help with writing integration tests using clj-webdriver."
   :url "https://github.com/AvisoNovate/taxi-toolkit"
   :license {:name "Apache Software License 2.0"
